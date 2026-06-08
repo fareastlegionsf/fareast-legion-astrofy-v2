de51367812357f5bd76cfeac4e3816fd63658eef
fareastlegionsf <fareastlegion.parasf@gmail.com>
2026-06-09 04:58:43 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
+site: "https://subtle-quokka-4d8c8e.netlify.app",
   integrations: [mdx(), sitemap(), tailwind()]
 });
