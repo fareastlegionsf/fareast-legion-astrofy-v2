8a409fbbf1cdc9c876aff8c8d7df043a3ebf286d
fareastlegionsf <fareastlegion.parasf@gmail.com>
2026-06-09 05:06:09 +0800

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-site: "https://subtle-quokka-4d8c8e.netlify.app",
-  integrations: [mdx(), sitemap(), tailwind()]
+  site: "https://subtle-quokka-4d8c8e.netlify.app",
+  integrations: [mdx(), tailwind()]
 });
