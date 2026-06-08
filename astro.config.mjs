182d2cca4c50b60d4c65ea6a127d874940763b71
fareastlegionsf <fareastlegion.parasf@gmail.com>
2026-06-09 05:14:43 +0800

    Add files via upload

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://subtle-quokka-4d8c8e.netlify.app",
   integrations: [mdx(), tailwind()]
